9246019039c9efad0fa53aa4560b85c3521680df
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-08-10 20:21:45 +0000

    update

--- pkg/sshd.go
+++ pkg/sshd.go
@@ -16,27 +16,53 @@ import (
 )
 
 
-func PublicKeyCallback(c ssh.ConnMetadata, pubKey ssh.PublicKey)(*ssh.Permissions, error){
-	authorizedKeysBytes, err := ioutil.ReadFile("authorized_keys")
-	if err != nil {
+type SSHD struct  {
+    authorizedKeysMap map[string]bool
+}
 
-		return nil, fmt.Errorf("can't read authorized_keys")
+func NewSSHD() *SSHD {
+    return &SSHD{}
+}
+
+// Callback
+func (s *SSHD)PublicKeyCallback(c ssh.ConnMetadata, pubKey ssh.PublicKey)(*ssh.Permissions, error) {
+
+		if s.authorizedKeysMap[string(pubKey.Marshal())] {
+			return &ssh.Permissions{
+				// Record the public key used for authentication.
+				Extensions: map[string]string{
+					"pubkey-fp": ssh.FingerprintSHA256(pubKey),
+				},
+			}, nil
+		}
+		return nil, fmt.Errorf("unknown public key for %q", c.User())
 	}
 
-	authorizedKeysMap := map[string]bool{}
+
+// ReadKey ... we read the key
+func (s *SSHD)ReadKey() error {
+
+
+	authorizedKeysBytes, err := ioutil.ReadFile("/etc/authorized_keys")
+	if err != nil {
+
+		return fmt.Errorf("can't read authorized_keys")
+	}
+
+	s.authorizedKeysMap = map[string]bool{}
 	for len(authorizedKeysBytes) > 0 {
 		pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
 		if err != nil {
 			log.Fatal(err)
-			return nil, fmt.Errorf("can't read authorized_keys")
 		}
 
-		authorizedKeysMap[string(pubKey.Marshal())] = true
+		s.authorizedKeysMap[string(pubKey.Marshal())] = true
 		authorizedKeysBytes = rest
 		
 	}
-	return nil, nil
+	return nil
 }
+
 
 
 
